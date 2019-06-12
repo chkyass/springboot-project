90124f15731e27e19be7c6305d059691c9c247a0
rand0m <you@example.com>
2019-06-11 20:35:57 -0700

    changing api path and default page size

--- src/main/java/chkyass/springbootproject/dao/EmployeeRepository.java
+++ src/main/java/chkyass/springbootproject/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package chkyass.springbootproject.dao;
 
 import chkyass.springbootproject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
